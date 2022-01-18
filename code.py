fd8dd9afaa32c5665cc5ec9f513e37ebb69fd21a
Sammy Dods <sammysdods@gmail.com>
2022-01-18 18:27:36 -0500

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ def are_valid_groups(student_numbers, groups):
     is_in_group = [False]*len(student_numbers)
     repeating = []
     for group in groups:
+        x = 1
         i = 0
         for student_number in student_numbers:
             if student_number in group and is_in_group[i] == True:
@@ -10,6 +11,9 @@ def are_valid_groups(student_numbers, groups):
             if student_number in group and is_in_group[i] == False:
                 is_in_group[i] = True
             i = i + 1
+            x = x + 1
+        if x < 2 or x > 3:
+            return False
     if True in is_in_group:
         return False
     elif True in repeating:
