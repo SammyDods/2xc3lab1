20c5761d7f7d756411ba95e7aade9aa310283d98
Sammy Dods <sammysdods@gmail.com>
2022-01-18 16:16:14 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
-def are_valid_groups(student_numbers[], groups[]):
+def are_valid_groups(student_numbers, groups):
+    dict = {}
+    for x in student_numbers:
+        dict[x] = 0
+    for x in groups:
+        if dict[x] == 1:
+            return False
+        else:
+            dict[x] = 1
+
+    return True
