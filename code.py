79229bb749c0716f81daf0ebca4018fed16bac24
retro2408 <choprab@mcmaster.ca>
2022-01-18 18:18:52 -0500

    Player 2 commiting

--- code.py
+++ code.py
@@ -1,9 +1,16 @@
 def are_valid_groups(student_numbers, groups):
 
+    Valid_Group_sizes = [2,3]
     is_in_group = [False]*len(student_numbers)
-    repeating = []
+    valid_groups = []
+    boolean_list1 = []
+    
     for group in groups:
         i = 0
+        if (len(group) in Valid_Group_sizes):
+            valid_groups.append(True)
+        else:
+            valid_groups.append(False)
         for student_number in student_numbers:
             if student_number in group and is_in_group[i] == True:
                 repeating.append(True)
@@ -12,7 +19,9 @@ def are_valid_groups(student_numbers, groups):
             i = i + 1
     if True in is_in_group:
         return False
-    elif True in repeating:
+    elif True in boolean_list1:
+        return False
+    elif False in valid_groups:
         return False
     else: return True
 
