34445b4485e8762dd404680a9794fd6c686687bd
Sammy Dods <sammysdods@gmail.com>
2022-01-18 18:33:05 -0500

    Revert "Merge branch 'main' of https://github.com/SammyDods/2xc3lab1 into main"
    This reverts commit 4157548764e31f2f9a833e8e4bf435e6c52f62cd, reversing
    changes made to fd8dd9afaa32c5665cc5ec9f513e37ebb69fd21a.

--- code.py
+++ code.py
@@ -1,17 +1,24 @@
 def are_valid_groups(student_numbers, groups):
 
-    is_in_group = [True]*len(student_numbers)
+    is_in_group = [False]*len(student_numbers)
     repeating = []
     for group in groups:
-
         x = 1
         i = 0
-        
+        for student_number in student_numbers:
+            if student_number in group and is_in_group[i] == True:
+                repeating.append(True)
+            if student_number in group and is_in_group[i] == False:
                 is_in_group[i] = True
             i = i + 1
             x = x + 1
         if x < 2 or x > 3:
             return False
+    if True in is_in_group:
+        return False
+    elif True in repeating:
+        return False
+    else: return True
 
 
 
