f67d06cab9dd2be70336c20c02a8f89cfcdd0174
retro2408 <choprab@mcmaster.ca>
2022-01-18 18:08:08 -0500

    Refreshing the Code For Player 2

--- code.py
+++ code.py
@@ -2,12 +2,8 @@ def are_valid_groups(student_numbers, groups):
 
     is_in_group = [False]*len(student_numbers)
     repeating = []
-    groups_2_3 = []
     for group in groups:
         i = 0
-        if len(group) >= 2 and len(group) <= 3:
-            groups_2_3.append(True)
-        else: groups_2_3.append(False)
         for student_number in student_numbers:
             if student_number in group and is_in_group[i] == True:
                 repeating.append(True)
@@ -18,8 +14,6 @@ def are_valid_groups(student_numbers, groups):
         return False
     elif True in repeating:
         return False
-    elif False in groups_2_3:
-        return False       
     else: return True
 
 
